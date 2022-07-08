3212ecc743ac9f4cc90d1cb7505d07e78fe50f72
Cristian <cristianrisueo@gmail.com>
2022-07-08 19:28:36 +0200

    First react router completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+// React components
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// Application components
 import { App } from "./components/app/App";
+
+// Styles
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/routes/navigation-bar/navigationBar.jsx
@@ -0,0 +1,26 @@
+// React components
+import { Outlet, Link } from "react-router-dom";
+
+// Logo component
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+// Styles
+import "./navigationBar.scss";
+
+export const NavigationBar = () => {
+  return (
+    <>
+      <div className="navigation-bar">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="links-container">
+          <Link className="link" to="shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
