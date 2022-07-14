466059e9c52b2adc987020323b9dba85b5f704b2
Cristian <cristianrisueo@gmail.com>
2022-07-14 13:17:25 +0200

    Routing on the home page categories completed

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -4,7 +4,41 @@ import { Category } from "../category/category";
 // Styles
 import { CategoriesContainerStyles } from "./categories.styles";
 
-export const Categories = ({ categories }) => {
+// Categories array
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
+  },
+];
+
+export const Categories = () => {
   return (
     <CategoriesContainerStyles>
       {/* Categories render */}
--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -1,9 +1,18 @@
+// React components
+import { useNavigate } from "react-router-dom";
+
 // Styles
 import "./category.scss";
 
 export const Category = ({ category }) => {
+  // useNavigate Hook
+  const navigate = useNavigate();
+
+  // Implementation of the hook
+  const goToCategory = () => navigate(category.route);
+  
   return (
-    <div className="category-item-container">
+    <div className="category-item-container" onClick={goToCategory}>
       <div
         className="category-image"
         style={{ backgroundImage: `url(${category.imageUrl})` }}
